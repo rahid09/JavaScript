22558ae3667b3591b682447b74d06e83195e5505
Rahid <rahidxlilov68@gmail.com>
2025-08-31 16:49:29 +0400

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,10 @@
+
+
+for (let i = 0; i < 5; i++) {
+  console.log("Sayı: " + i);
+}
+
+let arr = [10, 20, 30, 40, 50];
+for (let i = 0; i < arr.length; i++) {
+  console.log("Dizi Elemanı: " + arr[i]);
+}
